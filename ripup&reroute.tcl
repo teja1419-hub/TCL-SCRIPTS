cc3f13edc8216c8e7572fe9cd1862ab86c0f9374
SaiTeja <151114890+teja1419-hub@users.noreply.github.com>
2023-11-24 13:14:06 +0530

    Update ripup&reroute.tcl

--- ripup&reroute.tcl
+++ ripup&reroute.tcl
@@ -1,16 +1,24 @@
-if { [sizeof_collection [get_drc_errors -filter {type_name == "Short"} -quiet -error_data fpu_wrapper_lvs.err]] > 0 } {
+if { [sizeof_collection [get_drc_errors -filter {type_name == "Short"} -quiet -error_data pinmux_top_lvs.err]] > 0 } {
 
-	set all_shorted_nets [filter_collection [get_attribute [get_drc_errors -filter {type_name == "Short"} -quiet -error_data fpu_wrapper_lvs.err] objects ] "net_type == signal" ]
+	set all_shorted_nets [filter_collection [get_attribute [get_drc_errors -filter {type_name == "Short"} -quiet -error_data pinmux_top_lvs.err] objects ] "net_type == signal" ]
 	set num_short_nets [sizeof_collection $all_shorted_nets]
 	echo "Number of shorts $num_short_nets\n"
 	echo "note: all shorted nets are deleted except clock, power & ground nets and rerouted for DRC clean up \n"
 	if { $num_short_nets > 0 } {
 		remove_routes -net_types signal -detail_route -nets $all_shorted_nets
-		route_eco -nets [get_nets $all_shorted_nets]
+		
+		check_lvs -max_errors 0 -open_reporting detailed
 
-		#incremental cleanup
-		route_detail -initial_drc_from_input false -incremental true
-	}
+if { [sizeof_collection [get_drc_errors -filter {type_name == "Open"} -quiet -error_data pinmux_top_lvs.err]] > 0 } {
+
+	set all_open_nets [filter_collection [get_attribute [get_drc_errors -filter {type_name == "Open"} -quiet -error_data pinmux_top_lvs.err] objects ] "net_type == signal" ]
+
+	route_eco -nets [get_nets $all_open_nets]
+	#incremental cleanup
+	route_detail -initial_drc_from_input false -incremental true
+
 }
 
+	}
+}
 
